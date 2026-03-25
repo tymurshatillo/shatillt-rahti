fb24d1c1da532e7ec1dbc286bab731c3305e2358
SuperSlayer <146993255+SuperSlayer2@users.noreply.github.com>
2026-03-25 15:22:40 +0200

    IP thing

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return { "msg": "Hi" }
-
+@app.get("/api/ip")
+def read_root(request : Request):
+    return {"client_ip": request.client.host}
 
 @app.get("/items/{id}")
 def read_item(item_id: int, q: str = None):
-    return {"id": id, "q": q}
+    return {"id": id, "q": q}   
